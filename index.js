4d8588ccdeab6f8d05115a4b96255cba3c12b803
Varlotte <32116877+Varlotte@users.noreply.github.com>
2023-08-01 16:09:47 -0400

    passes 9 tests

--- index.js
+++ index.js
@@ -1,11 +1,34 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(a) {
+  if (a <= 400) {
+    return "This one is on me!";
+  }
+
+  if (a >= 401 && a <= 2000) {
+    return "That will be twenty bucks.";
+  }
+
+  if (a >= 2001 && a <= 2499) {
+    return "I will gladly take your thirty bucks.";
+  }
+
+  if (a >= 2500) {
+    return "No can do.";
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city) {
+  return city === "NYC" ? "Ok, sounds good." : "No go.";
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip) {
+  switch (tip) {
+    case "generous":
+      return "Thank you so much.";
+      break;
+    case "not as generous":
+      return "Thank you.";
+      break;
+    default:
+      return "Bye.";
+  }
 }
